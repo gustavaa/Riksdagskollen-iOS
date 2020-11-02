411de3cae811d3a03a44871f33d31add6c3b3ae1
Gustav Aaro <gustav.aaro@gmail.com>
2020-11-02 15:26:07 +0100

    Initial Commit

---
+++ Riksdagskollen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Riksdagskollen
+//
+//  Created by Gustav Aaro on 2020-11-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
